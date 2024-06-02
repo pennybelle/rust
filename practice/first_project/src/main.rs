c27fe0a44ad1822827876bdc986c5872cf2407c9
cake <3rdbinarybug@gmail.com>
2024-06-01 20:04:34 -0400

    Fix main.rs
    Gonna take a while to get used to all these semicolons...

--- practice/first_project/src/main.rs
+++ practice/first_project/src/main.rs
@@ -3,8 +3,23 @@ fn main() {
     println!("Hello, world! This is a test. Now im testing the cargo run command >:3");
 
     // v0.1.1
+    println!("---");
     let mut x = 4;
     println!("x = {}", x);
     x = 5;
-    println!("x now = {}", x)
+    println!("x now = {}", x);
+
+    // v0.1.2
+    println!("---");
+    let x = 4;
+    println!("x = {}", x);
+    let x = 5;
+    println!("x now = {}", x);
+
+    // v0.1.3
+    println!("---");
+    let x = 4;
+    println!("x = {}", x);
+    let x = x + 1;
+    println!("x now = {}", x);
 }
