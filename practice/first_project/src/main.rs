7f51ab001ba815950ab44c1919a3fee6c684ed4c
cake <3rdbinarybug@gmail.com>
2024-06-01 21:45:54 -0400

    Update main.rs
    Learning types of variables such as int vs float and the different binary options of each type. Still not sure how they work yet tho..

--- practice/first_project/src/main.rs
+++ practice/first_project/src/main.rs
@@ -2,15 +2,15 @@ fn main() {
     // v0.1.0
     println!("Hello, world! This is a test. Now im testing the cargo run command >:3");
 
-    // v0.1.1
+    // v0.1.1 learning about variables
     println!("---");
-    
+
     let mut x = 4;
     println!("x = {}", x);
     x = 5;
     println!("x now = {}", x);
 
-    // v0.1.2
+    // v0.1.2 more variables
     println!("---");
 
     let x = 4;
@@ -18,7 +18,7 @@ fn main() {
     let x = 5;
     println!("x now = {}", x);
 
-    // v0.1.3
+    // v0.1.3 and more
     println!("---");
 
     let x = 4;
@@ -26,7 +26,7 @@ fn main() {
     let x = x + 1;
     println!("x now = {}", x);
 
-    // v0.1.4
+    // v0.1.4 learning about scopes
     println!("---");
 
     let x = 4;
@@ -35,5 +35,22 @@ fn main() {
         let x = 5;
         println!("x = {}", x);
     };
+    {
+        let x = "hi there";
+        println!("x = {}", x);
+    };
+    println!("x = {}", x);
+
+    // v0.1.5 difference between int and float
+    println!("---");
+
+    let x: u32 = 1000; // int (only positive)
+    println!("x = {}", x);
+    let x: i32 = 1000; // int (positive or negative)
+    println!("x = {}", x);
+
+    let x: f32 = 1000.0; // float (single precision)
+    println!("x = {}", x);
+    let x: f64 = 1000.0; // float (single precision)
     println!("x = {}", x);
 }
