f9cf8f3a07a73f1c5254f47ea68446d382f0173b
cake <3rdbinarybug@gmail.com>
2024-06-01 22:03:49 -0400

    Update main.ms
    Moved all my learning iterations to functions denoted by versions so i can choose to turn them off, and i wanted a cleaner main func

--- practice/first_project/src/main.rs
+++ practice/first_project/src/main.rs
@@ -1,15 +1,19 @@
-fn main() {
+fn v010() {
     // v0.1.0
     println!("Hello, world! This is a test. Now im testing the cargo run command >:3");
+}
 
-    // v0.1.1 learning about variables
+fn v011() {
+    // v0.1.1
     println!("---");
 
     let mut x = 4;
     println!("x = {}", x);
     x = 5;
     println!("x now = {}", x);
+}
 
+fn v012() {
     // v0.1.2 more variables
     println!("---");
 
@@ -17,7 +21,9 @@ fn main() {
     println!("x = {}", x);
     let x = 5;
     println!("x now = {}", x);
+}
 
+fn v013() {
     // v0.1.3 and more
     println!("---");
 
@@ -25,7 +31,9 @@ fn main() {
     println!("x = {}", x);
     let x = x + 1;
     println!("x now = {}", x);
+}
 
+fn v014() {
     // v0.1.4 learning about scopes
     println!("---");
 
@@ -40,17 +48,28 @@ fn main() {
         println!("x = {}", x);
     };
     println!("x = {}", x);
+}
 
+fn v015() {
     // v0.1.5 difference between int and float
     println!("---");
 
-    let x: u32 = 1000; // int (only positive)
+    let x: u32 = 1000; // int (only positive) (default)
     println!("x = {}", x);
     let x: i32 = 1000; // int (positive or negative)
     println!("x = {}", x);
 
     let x: f32 = 1000.0; // float (single precision)
     println!("x = {}", x);
-    let x: f64 = 1000.0; // float (single precision)
+    let x: f64 = 1000.0; // float (double precision) (default)
     println!("x = {}", x);
+}
+
+fn main() {
+    // v010();
+    // v011();
+    // v012();
+    // v013();
+    // v014();
+    v015();
 }
