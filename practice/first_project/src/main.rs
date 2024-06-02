a1906c67ccb6311cc83cf6b9fde4b63b3f1474d4
cake <3rdbinarybug@gmail.com>
2024-06-01 21:02:17 -0400

    Update main.rs
    Testing scope behavior

--- practice/first_project/src/main.rs
+++ practice/first_project/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
 
     // v0.1.1
     println!("---");
+    
     let mut x = 4;
     println!("x = {}", x);
     x = 5;
@@ -11,6 +12,7 @@ fn main() {
 
     // v0.1.2
     println!("---");
+
     let x = 4;
     println!("x = {}", x);
     let x = 5;
@@ -18,8 +20,20 @@ fn main() {
 
     // v0.1.3
     println!("---");
+
     let x = 4;
     println!("x = {}", x);
     let x = x + 1;
     println!("x now = {}", x);
+
+    // v0.1.4
+    println!("---");
+
+    let x = 4;
+    println!("x = {}", x);
+    {
+        let x = 5;
+        println!("x = {}", x);
+    };
+    println!("x = {}", x);
 }
---
+++ practice/first_project/target/debug/deps/first_project-0d4a85c0671d95ad.d
@@ -0,0 +1,5 @@
+/home/cake/Documents/GitHub/rust/practice/first_project/target/debug/deps/libfirst_project-0d4a85c0671d95ad.rmeta: src/main.rs
+
+/home/cake/Documents/GitHub/rust/practice/first_project/target/debug/deps/first_project-0d4a85c0671d95ad.d: src/main.rs
+
+src/main.rs:
