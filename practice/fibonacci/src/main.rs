15fbb847f84f96f23c19776a0533fcb936942f2d
pennybelle <kylanfreeman98@gmail.com>
2024-07-27 06:18:47 -0400

    removed cache

--- practice/fibonacci/src/main.rs
+++ practice/fibonacci/src/main.rs
@@ -2,15 +2,12 @@ fn main() {
     fibonacci(25);
 }
 
-fn fibonacci(depth: u64) {
+fn fibonacci(depth: i64) {
     let mut last: u64 = 0;
     let mut current: u64 = 1;
 
     for _ in 0..depth {
         println!("{}", last);
-
-        let cache: u64 = current + last;
-        last = current;
-        current = cache;
+        (last, current) = (current, current + last);
     }
 }
